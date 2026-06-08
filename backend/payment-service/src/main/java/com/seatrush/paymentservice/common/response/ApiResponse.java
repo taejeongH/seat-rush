383997d5d20679854da7e5ed70ef453a8a841e87
htj7613 <htj7613@naver.com>
2026-06-08 22:02:54 +0900

    chore: 공통 api 응답 구조 설정

---
+++ backend/payment-service/src/main/java/com/seatrush/paymentservice/common/response/ApiResponse.java
@@ -0,0 +1,65 @@
+package com.seatrush.paymentservice.common.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.seatrush.paymentservice.common.response.status.ErrorCode;
+import com.seatrush.paymentservice.common.response.status.SuccessCode;
+import org.springframework.http.ResponseEntity;
+
+public record ApiResponse<T>(
+        Boolean isSuccess,
+        String code,
+        String message,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        T result
+) {
+
+    public static <T> ResponseEntity<ApiResponse<T>> onSuccess(SuccessCode code, T result) {
+        ApiResponse<T> body = new ApiResponse<>(
+                code.isSuccess(),
+                code.getCode(),
+                code.getMessage(),
+                result
+        );
+        return ResponseEntity.status(code.getHttpStatus()).body(body);
+    }
+
+    public static ResponseEntity<ApiResponse<Void>> onSuccess(SuccessCode code) {
+        ApiResponse<Void> body = new ApiResponse<>(
+                code.isSuccess(),
+                code.getCode(),
+                code.getMessage(),
+                null
+        );
+        return ResponseEntity.status(code.getHttpStatus()).body(body);
+    }
+
+    public static ResponseEntity<ApiResponse<Void>> onFailure(ErrorCode code) {
+        ApiResponse<Void> body = new ApiResponse<>(
+                code.isSuccess(),
+                code.getCode(),
+                code.getMessage(),
+                null
+        );
+        return ResponseEntity.status(code.getHttpStatus()).body(body);
+    }
+
+    public static ResponseEntity<ApiResponse<Void>> onFailure(ErrorCode code, String message) {
+        ApiResponse<Void> body = new ApiResponse<>(
+                code.isSuccess(),
+                code.getCode(),
+                message,
+                null
+        );
+        return ResponseEntity.status(code.getHttpStatus()).body(body);
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> onFailure(ErrorCode code, String message, T result) {
+        ApiResponse<T> body = new ApiResponse<>(
+                code.isSuccess(),
+                code.getCode(),
+                message,
+                result
+        );
+        return ResponseEntity.status(code.getHttpStatus()).body(body);
+    }
+}
